0283806c338d442a9931ca5cef552db4580e08c3
shubhamsahu <sahu4816@gmail.com>
2021-02-11 14:04:03 +0530

    new2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <p>I am learing React. My life is getting better.</p>
+      <p>I am learning React. My life is getting better. </p>
     </div>
   );
 }
